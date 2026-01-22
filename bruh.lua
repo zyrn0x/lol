faf1f08adbea875b9a289e6a4bb1023cd4baa558
zyrn0x <adambenkhai34@gmail.com>
2026-01-22 17:51:36 +0100

    Update bruh.lua

--- bruh.lua
+++ bruh.lua
@@ -1,4 +1,28 @@
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
 
+local namecall = mt.__namecall
+
+mt.__namecall = newcclosure(function(self, ...)
+   local method = getnamecallmethod()
+   local args = {...}
+
+   if method == "FireServer" then
+       if tostring(self) == "SettingEvent" then
+           if args[1] == "CL" then
+               return;
+           end
+       elseif tostring(self) == "KickEvent" then
+           return;
+       elseif tostring(self) ~= "KickEvent" and tostring(self) ~= "SettingEvent" then
+            return namecall(self, table.unpack(args))
+       end
+   else
+       return namecall(self, table.unpack(args))
+   end
+end)
+
+print("Loaded Anti-Cheat Bypass!");
 getgenv().GG = {
     Language = {
         CheckboxEnabled = "Enabled",
@@ -18,11 +42,6 @@ getgenv().GG = {
         Success = "Success"
     }
 }
-
-game:GetService("ReplicatedStorage").Security.RemoteEvent:Destroy()
-game:GetService("ReplicatedStorage").Security[""]:Destroy()
-game:GetService("ReplicatedStorage").Security:Destroy()
-game:GetService("Players").LocalPlayer.PlayerScripts.Client.DeviceChecker:Destroy()
 
 -- Replace the SelectedLanguage with a reference to GG.Language
 local SelectedLanguage = GG.Language
